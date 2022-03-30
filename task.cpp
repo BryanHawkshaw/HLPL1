a4ffc296b6a906f42029293abfa8c336a72f3b3a
BryanHawkshaw <67053814+BryanHawkshaw@users.noreply.github.com>
2022-03-30 09:09:38 +0200

    Add files via upload

---
+++ task.cpp
@@ -0,0 +1,33 @@
+#include "std_lib_facilities.h"
+class Complex{
+    public:
+        complex number : complex();
+        complex number (double re, double im);
+        double modulus (Complex C); {
+            re, im = C;
+        }
+        C() re{1}, im{2};
+        double getNumber(double){
+        return re, im;
+    }
+
+    private:
+        double re;
+        double im;
+};
+
+    void() {
+        complex () :: double modulus(Complex c)
+    }
+
+Complex operator+ {
+    cout << re + im << endl;
+}
+
+Complex operator<< {
+    cout << re 
+}
+int main() {
+    modulus :: scope resolution operator;
+    return 0;
+}
